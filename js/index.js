2c1d37f6445888ba1d62f313e1dfcee0062a162f
Svetlana <ss504@list.ru>
2020-08-23 17:25:28 +0300

    add list

--- js/index.js
+++ js/index.js
@@ -16,3 +16,42 @@ for(let el of tabLinks) {
         panel[0].classList.add("active");
     });
 }
+function DropDown(el) {
+    this.dd = el;
+    this.placeholder = this.dd.children('span');
+    this.opts = this.dd.find('ul.dropdown > li');
+    this.val = '';
+    this.index = -1;
+    this.initEvents();
+}
+DropDown.prototype = {
+    initEvents : function() {
+        var obj = this;
+
+        obj.dd.on('click', function(event){
+            $(this).toggleClass('active');
+            return false;
+        });
+
+        obj.opts.on('click',function(){
+            var opt = $(this);
+            obj.val = opt.text();
+            obj.index = opt.index();
+            obj.placeholder.text(obj.val);
+        });
+    },
+    getValue : function() {
+        return this.val;
+    },
+    getIndex : function() {
+        return this.index;
+    }
+}
+$(function() {
+
+    var dd = new DropDown( $('#dd') );
+
+    $(document).click(function() {
+        $('.wrapper-dropdown-1').removeClass('active');
+    });
+});
